3c1e098925dd67dc1eabe4be76663995a0b6ef9b
Tanaka I Kahwai <iankahwai@gmail.com>
2022-10-28 08:35:43 +0200

    Optimize transitions

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Container({ children }) {
     return (
-        <div className='bg-gray-50 w-full min-h-screen p-1 md:p-10 space-y-4'>
+        <div className='bg-gray-50 transition-all duration-1000 w-full min-h-screen p-1 md:p-10 space-y-4'>
             {children}
         </div>
     );
--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { RootObject } from '../pages/Overview';
 
 export function TableComponent(rowSelection: { selectedRowKeys: any[]; onChange: (newSelectedRowKeys: React.Key[]) => void; }, filteredData: any[], columns: ({ title: string; render: (record: RootObject) => JSX.Element; dataIndex?: undefined; } | { title: string; dataIndex: string; render?: undefined; })[], hidden: boolean = true, onRow: (record, index)=>void = ()=>{}) {
-    return <div className="w-full bg-white overflow-x-scroll md:overflow-hidden md:w-full my-6">
+    return <div className="w-full bg-white overflow-x-scroll md:overflow-hidden md:w-full my-6 transition-all duration-1000">
         <Table
             rowSelection={rowSelection}
             pagination={{ position: ['bottomCenter'] }}
